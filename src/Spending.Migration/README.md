bd04f152ada60c7e58255ef0e5fc694091ebe79f
kevinmoutonbizcover <kevinm@bizcover.com.au>
2019-11-29 14:22:04 +1100

    add secret

--- src/Spending.Migration/README.md
+++ src/Spending.Migration/README.md
@@ -6,5 +6,19 @@ Install Microsoft.EntityFrameworkCore.Tools to be able to use the Package Manage
 
 Add-Migration InitialCreate
 
+# Secrets
+
+## Init project
+
+dotnet user-secrets init
+
+## Add secret
+
+dotnet user-secrets set "ConnectionStrings:SpendingDatabase" "..."
+
+## See secrets
+
+Right click on the project then "Manage User Secrets"
+
 
 
--- src/Spending.Migration/Startup.cs
+++ src/Spending.Migration/Startup.cs
@@ -1,28 +1,22 @@
 ﻿using System.IO;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Spending.Models;
 
 namespace Spending.Migration
 {
-    public class MigrationContext : DbContext
+    public class Startup
     {
-        public DbSet<Bank> Bank { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddEnvironmentVariables();
-
+            
             builder.AddUserSecrets<Startup>();
 
             var configuration = builder.Build();
 
             var spendingDatabaseConnectionString = configuration["ConnectionStrings:SpendingDatabase"];
-
-            optionsBuilder.UseSqlServer(spendingDatabaseConnectionString);
         }
     }
 }
